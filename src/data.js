21dba904bff7d125d3959328074883c59939dade
Bacho19 <bacho.usubovi@gmail.com>
2021-01-24 20:00:53 +0400

    data adding

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const people = [
+    {
+        id: 1,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1LQiMC4nbDOvjRK_rk0mTNaD8oWmvuoh8Aw&usqp=CAU',
+        name: 'Whoever Know',
+        title: 'Developer',
+        desc: 'as/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jdas/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jdas/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jd'
+    },
+    {
+        id: 2,
+        image: 'https://banner2.cleanpng.com/20180519/lth/kisspng-emoji-android-oreo-noto-fonts-github-5affe50d23dcf6.6839431815267197571469.jpg',
+        name: 'Tom Harison',
+        title: 'Manager',
+        desc: 'as/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jdas/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jdas/d kaj'
+    },
+    {
+        id: 3,
+        image: 'https://cdn.imgbin.com/13/1/6/imgbin-angry-face-iphone-emoji-android-anger-angry-emoji-qjYNDJcreZ2cFdnEyFzw5dZFw.jpg',
+        name: 'John Johnson',
+        title: 'Marketing',
+        desc: 'as/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jdas/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jdas/d kajh lajdl;a jda;lsj dasl;j al;j'
+    },
+    {
+        id: 4,
+        image: 'https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F5056fc16-4b1e-4281-9de3-bb0d5f57c597_512x512.png',
+        name: 'Know Whoever',
+        title: 'Idk',
+        desc: 'as/d kajh lajdl;a jda;lsj dasl;j al;j d;al ja;l j;al jdas/d kajh lajdl;a jda;lsj dasl;j'
+    }
+]
